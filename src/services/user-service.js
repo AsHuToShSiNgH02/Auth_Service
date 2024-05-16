6126c5b7e2800aac0cbd867321b00ca3b3bb006f
AsHuToShSiNgH02 <ashutoshin2002@gmail.com>
2024-05-16 15:56:52 +0530

    Added email and password validation

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt'); 
 
 const UserRepository = require('../repository/user-repository');
 const { JWT_KEY } = require('../config/serverConfig');
