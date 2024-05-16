58b09048b969fdf70c548564538a2e9f3c920cb1
AsHuToShSiNgH02 <ashutoshin2002@gmail.com>
2024-05-16 23:09:14 +0530

    Added validation error class

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt'); 
+const bcrypt = require('bcrypt');
+const AppErrors = require('../utils/error-handler'); 
 
 const UserRepository = require('../repository/user-repository');
 const { JWT_KEY } = require('../config/serverConfig');
@@ -14,6 +15,9 @@ class UserService {
             const user = await this.userRepository.create(data);
             return user;
         } catch (error) {
+            if(error.name == 'SequelizeValidationError') {
+                throw error;
+            }
             console.log("Something went wrong in the service layer");
             throw error;
         }
